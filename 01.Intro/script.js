482802eafe32ddf6e9907b5e5f29e898963e3e0b
Həsənli Firidun <firidunhesenli112@gmail.com>
2025-10-20 20:28:50 +0400

    first commit

---
+++ 01.Intro/script.js
@@ -0,0 +1,18 @@
+// Types
+// Primitives
+let a = 42; // number
+let b = `Hello, World!`; // string
+let c = true; // boolean
+let d = null; // null
+let e; // undefined
+
+// Non-primitives
+let f = [1, 2, 3]; // array
+let g = { name: "Alice", age: 30 }; // object
+
+// Functions
+function add(x, y) {
+    return x + y;
+}
+
+add(5, 10);
